1f1635346e31f2a3d66edac81b0b101d077705d9
Emre Güler <guler.emre.99@gmail.com>
2023-12-01 16:11:22 +0300

    Initial commit

---
+++ dev-emreguler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dev_emreguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
